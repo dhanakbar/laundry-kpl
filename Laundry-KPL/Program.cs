0b37440feb541a4330a33e4f450517cb7b264c18
dhanakbar <maulana261100@gmail.com>
2021-06-10 17:02:35 +0800

    Menambahkan Form input Jasa

--- Laundry-KPL/Program.cs
+++ Laundry-KPL/Program.cs
@@ -14,9 +14,12 @@ namespace Laundry_KPL
         [STAThread]
         static void Main()
         {
+            /*Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());*/
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new InputJasa());
         }
     }
 }
